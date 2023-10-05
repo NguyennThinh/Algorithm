e1a1111294e998fb21b2289ea15d4abc9ba9acaf
NguyennThinh <nguyenthinhc9@gmail.com>
2023-10-05 15:19:17 +0700

    LetCode 2798

---
+++ src/LettCode/LetCode2798.java
@@ -0,0 +1,17 @@
+package LettCode;
+
+public class LetCode2798 {
+
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int ans = 0;
+        for (int i  =0 ; i < hours.length; i++){
+
+            if (hours[i] >= target){
+                ans++;
+            }
+
+        }
+
+        return ans;
+    }
+}
