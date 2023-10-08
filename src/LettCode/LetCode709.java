04bcafbc8ef9e4aa1659f17953ef8de5c240049f
NguyennThinh <nguyenthinhc9@gmail.com>
2023-10-08 19:51:14 +0700

    LetCode 709

---
+++ src/LettCode/LetCode709.java
@@ -0,0 +1,20 @@
+package LettCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetCode709 {
+
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) (c + 32);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+}
